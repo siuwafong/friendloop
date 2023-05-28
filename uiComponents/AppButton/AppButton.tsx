8ed44bc18120a47493118a91cefa14807c666af4
Wilson Fong <wilsonfong1002@outlook.com>
2023-05-28 16:45:22 -0400

    add more props to AppButton

--- uiComponents/AppButton/AppButton.tsx
+++ uiComponents/AppButton/AppButton.tsx
@@ -5,8 +5,17 @@ import { Button } from 'react-native-paper';
 interface AppButtonProps {
   onPress: (e: GestureResponderEvent) => void;
   text: string;
+  loading?: boolean;
 }
 
-export const AppButton: React.FC<AppButtonProps> = ({ onPress, text }) => {
-  return <Button onPress={onPress}>{text}</Button>;
+export const AppButton: React.FC<AppButtonProps> = ({
+  onPress,
+  text,
+  loading,
+}) => {
+  return (
+    <Button onPress={onPress} loading={loading ? loading : true}>
+      {text}
+    </Button>
+  );
 };
